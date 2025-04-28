1b2ab782dcf808eeb1edf6f1fe141cf23e896811
Zeynep Merve Koyuncu <sdfd2266@gmail.com>
2025-04-28 15:51:41 +0300

    index.html ve firebase config yüklendi

---
+++ main.js
@@ -0,0 +1,14 @@
+// Your Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyC2uuR8b3876Pq6HBKRHw8duPyPXUZQ2C0",
+  authDomain: "fir-giriscikis.firebaseapp.com",
+  projectId: "fir-giriscikis",
+  storageBucket: "fir-giriscikis.firebasestorage.app",
+  messagingSenderId: "134285846287",
+  appId: "1:134285846287:web:23a25df42c663f06e2007a"
+};
+
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+const auth = firebase.auth();
+ 
